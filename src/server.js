fbe73371ccf53ead0b139720d7191db6a3f46658
Phương Nguyễn <vietphuong461983@gmail.com>
2024-12-28 10:56:24 +0700

    create users

--- src/server.js
+++ src/server.js
@@ -2,16 +2,20 @@ import express from "express";
 import configViewEngine from "./configs/viewEngine";
 
 import initWebRouter from "./route/web";
-import connection from './configs/connectDB'
-
+import connection from "./configs/connectDB";
 
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8081;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// setup view engine
 configViewEngine(app);
 
+// init web route
 initWebRouter(app);
 
 app.get("/about", (req, res) => {
